8599e820010ec8aefb93ab624401967eed4c8687
Kathy <hatesjade@gmail.com>
2026-06-03 20:06:52 +0800

    remove line number

--- src/js/code-lines.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('pre code').forEach(function(block) {
-    var lines = block.innerHTML.split('\n');
-    if (lines[lines.length - 1] === '') lines.pop();
-    block.innerHTML = lines.map(function(line) {
-        return '<span class="line">' + (line || '​') + '</span>';
-    }).join('');
-});
